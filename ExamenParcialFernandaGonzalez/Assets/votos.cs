5b39b19ad44e5de24efb23f72ab42c378c719104
MariaRoj <106767462+MariaRoj@users.noreply.github.com>
2023-03-02 13:21:29 -0600

    termine

---
+++ ExamenParcialFernandaGonzalez/Assets/votos.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+
+public class votos : MonoBehaviour
+{
+    private int seleccion;
+    [SerializeField] private TMP_Text IAtexto;
+    [SerializeField] private TMP_Text Vototexto;
+  int favor = 0;
+    int contra = 0;
+    
+
+    
+    
+        
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Mdc()
+    {
+        if
+    }
+            
+   
+}
